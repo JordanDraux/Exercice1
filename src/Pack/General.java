d0f2d3567a03e6657f7d490dd9db2d8666443bbe
JordanDraux <jordan.draux@hotmail.com>
2015-10-16 15:03:24 +0200

    Retour en arrière

--- src/Pack/General.java
+++ src/Pack/General.java
@@ -4,18 +4,13 @@ public class General
 {
 	public static void main(String[] args) 
 	{
-		BizzBuzz(3);
+		Bizz(6);
+		
 	}
 	
-	static void BizzBuzz(int n)
+	static void Bizz(int n)
 	{
 		for(int x=0; x<100 ; x=x+3)
-		{
-		if(n==x)
-		System.out.println("Bisous mon chou");
-		}
-		
-		for(int x=0; x<100 ; x=x+5)
 		{
 		if(n==x)
 		System.out.println("Bisous mon chou");

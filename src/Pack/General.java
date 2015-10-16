75248f658a2fe2d8907f60f693c9dcb0286a494c
JordanDraux <jordan.draux@hotmail.com>
2015-10-16 14:47:24 +0200

    Modif fonction BizzBuzz

--- src/Pack/General.java
+++ src/Pack/General.java
@@ -4,29 +4,17 @@ public class General
 {
 	public static void main(String[] args) 
 	{
-		int compteur = 0;
-		do
-		{
-			compteur++;
-			Bizz(compteur);
-			Buzz(compteur);
-			System.out.println(compteur);
-		}while(compteur<100);
-		
-		
+		BizzBuzz(3);
 	}
 	
-	static void Bizz(int n)
+	static void BizzBuzz(int n)
 	{
 		for(int x=0; x<100 ; x=x+3)
 		{
 		if(n==x)
 		System.out.println("Bisous mon chou");
 		}
-	}
-	
-	static void Buzz(int n)
-	{
+		
 		for(int x=0; x<100 ; x=x+5)
 		{
 		if(n==x)

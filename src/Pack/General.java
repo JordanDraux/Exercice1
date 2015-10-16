aafe291252e4f1a484728191a9f0343d0f972961
JordanDraux <jordan.draux@hotmail.com>
2015-10-16 14:44:49 +0200

    Ajout de la fonction Buzz

--- src/Pack/General.java
+++ src/Pack/General.java
@@ -4,13 +4,30 @@ public class General
 {
 	public static void main(String[] args) 
 	{
-		Bizz(6);
+		int compteur = 0;
+		do
+		{
+			compteur++;
+			Bizz(compteur);
+			Buzz(compteur);
+			System.out.println(compteur);
+		}while(compteur<100);
+		
 		
 	}
 	
 	static void Bizz(int n)
 	{
 		for(int x=0; x<100 ; x=x+3)
+		{
+		if(n==x)
+		System.out.println("Bisous mon chou");
+		}
+	}
+	
+	static void Buzz(int n)
+	{
+		for(int x=0; x<100 ; x=x+5)
 		{
 		if(n==x)
 		System.out.println("Bisous mon chou");
